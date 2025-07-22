badc730cb63fdc5283cf8417e9c06e8b90f6c5c2
Robin Rathore <robinsingh248142@gmail.com>
2025-07-22 20:16:42 +0530

    (fix): Fixed the category product page

--- src/pages/CategoryProducts/CategoryProducts.tsx
+++ src/pages/CategoryProducts/CategoryProducts.tsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 interface CategoryProductsProps {}
 
 export const CategoryProducts: React.FC<CategoryProductsProps> = () => {
   return (
     <div>
-      <h1>Category Products Page</h1>
+      <h1>Category Products Page!</h1>
     </div>
   );
 };
